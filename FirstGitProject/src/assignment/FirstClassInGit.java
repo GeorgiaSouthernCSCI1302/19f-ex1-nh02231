12782b46d64a48e32e085f188243e9254fa17d0d
nh02231 <nh02231@CEIT2553214X032.ad.georgiasouthern.edu>
2019-08-21 16:19:06 -0400

    +Added sout of my name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -4,6 +4,7 @@ public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Nathan Heilands");
 		
 		//add a statement to display your name in the console
 		

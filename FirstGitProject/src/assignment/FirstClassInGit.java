77e644f374a717fd95949c6836778b82c1d34807
nh02231 <nh02231@CEIT2553214X032.ad.georgiasouthern.edu>
2019-08-21 16:52:27 -0400

    +Rearranged class format +Added question to ask user's last name +Added BufferedReader class +Added input String +Added display statement of user's last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,37 @@
 package assignment;
 
-public class FirstClassInGit {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
+public class FirstClassInGit {
+	
+	private BufferedReader br;
+	private String input;
+
+	FirstClassInGit()
+	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			run();
+		} catch (IOException e) {
+			System.out.println("Failed to read input");
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	public void run() throws IOException
+	{
 		System.out.println("Hello my name is Nathan Heiland!");
+		System.out.println("What is your last name?");
+		input = br.readLine();
+		System.out.println("Your last name is "+input+".");
+	}
+	
+	public static void main(String[] args)
+	{
+		new FirstClassInGit();
 		
 		//add a statement to display your name in the console
 		
